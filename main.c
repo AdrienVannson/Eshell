84b655f22847a595b6fce65178b2edb090973d13
Adrien Vannson <adrien.vannson@protonmail.com>
2022-03-20 12:26:15 +0100

    Set a variable in the environnement

--- main.c
+++ main.c
@@ -83,6 +83,10 @@ int main()
             is_over = true;
         } else if (!strcmp(command_name, "env")) {
             print_env();
+        } else if (!strcmp(command_name, "export")) { // Set a variable
+            char* var_name = strtok(NULL, " =");
+            char* value = strtok(NULL, "\n");
+            set_value_to_env(var_name, value);
         } else if (!strcmp(command_name, "echo")) { // Show a variable
             char* var_name = strtok(NULL, TOKEN_SEPARATORS);
 
